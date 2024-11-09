ae403ac6ed528a7b62663b9ec21f792e3f480c3b
Renan <renanclaude.rcg@gmail.com>
2024-11-08 23:19:38 -0300

    Creating SignUpView

--- talk-backend/accounts/views.py
+++ talk-backend/accounts/views.py
@@ -24,14 +24,33 @@ class SignInView(APIView, Authentication):
         password = request.data.get("password", "")
 
         signin = self.signin(email, password)
-        
+
         if not signin:
             raise AuthenticationFailed
-        
+
         user = UserSerializer(signin).data
         access_token = RefreshToken.for_user(signin).access_token
 
-        return Response({
-            "user": user,
-            "access_token": str(access_token)
-        })
+        return Response({"user": user, "access_token": str(access_token)})
+
+
+class SignUpView(APIView, Authentication):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        name = request.data.get("name", "")
+        email = request.data.get("email", "")
+        password = request.data.get("password", "")
+
+        if not name or not email or not password:
+            raise AuthenticationFailed
+
+        signup = self.signup(name, email, password)
+
+        if not signup:
+            raise AuthenticationFailed
+
+        user = UserSerializer(signup).data
+        access_token = RefreshToken.for_user(signup).access_token
+
+        return Response({"user": user, "access_token": str(access_token)})
