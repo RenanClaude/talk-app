a7e4a520c8a5088dbb3baa9d131c2a9433e4b328
Renan <renanclaude.rcg@gmail.com>
2024-12-10 22:55:11 -0300

    Creating the SignUp page - Part 2 - Finalization

---
+++ talk-frontend/src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpPage } from "@/components/Pages/SignUp";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Registrar-se",
+};
+
+const SignUp = () => <SignUpPage />;
+
+export default SignUp;
