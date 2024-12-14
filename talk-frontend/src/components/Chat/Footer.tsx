ab7981878aa2f95d27db7c94859a13ecc284b80b
Renan <renanclaude.rcg@gmail.com>
2024-12-13 22:06:56 -0300

    Chat Footer - Part 2 - functions (handleStartingRecording, handleSendRecording, handleDeleteRecording, and handleSendMessage)

--- talk-frontend/src/components/Chat/Footer.tsx
+++ talk-frontend/src/components/Chat/Footer.tsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "next-themes";
 import { ChangeEvent, useRef, useState } from "react";
+import { toast } from "sonner";
 
 type ChatFooterProps = {
   onSendMessage: (data: {
@@ -20,7 +21,7 @@ export const ChatFooter = ({ onSendMessage }: ChatFooterProps) => {
 
   const theme = useTheme();
 
-  const mediaRercorder = useRef<MediaRecorder | null>(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
 
   const handleToggleEmojiPicker = () => setEmojiPicker(!emojiPicker);
 
@@ -34,7 +35,71 @@ export const ChatFooter = ({ onSendMessage }: ChatFooterProps) => {
     if (file) setMessageAttachment(file);
   };
 
-  const handleStartingRecording = () => {};
+  const handleStartingRecording = async () => {
+    let stream: MediaStream | null = null;
+
+    if ("MediaRercorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        stream = streamData;
+      } catch (error) {
+        toast.error("Habilite as permissões do microfone.", {
+          position: "top-center",
+        });
+        return;
+      }
+    } else {
+      toast.error("Seu navegador não tem suporte para gravação de áudio.", {
+        position: "top-center",
+      });
+      return;
+    }
+
+    if (!stream) return;
+    setIsRecording(true);
+    const media = new MediaRecorder(stream, { mimeType: "audio/webm" });
+    mediaRecorder.current = media;
+    mediaRecorder.current.start();
+
+    const localAudioChunks: Blob[] = [];
+
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined" || event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+
+    setAudioChunks(localAudioChunks);
+  };
+
+  const handleSendRecording = () => {
+    if (!mediaRecorder.current) return;
+    setIsRecording(false);
+    mediaRecorder.current.stop();
+
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+      onSendMessage({ audio: audioBlob });
+      setAudioChunks([]);
+    };
+
+    mediaRecorder.current.stream.getTracks().forEach((track) => track.stop());
+  };
+
+  const handleDeleteRecording = () => {
+    if (!mediaRecorder.current) return;
+    setIsRecording(false);
+    mediaRecorder.current.stop();
+    mediaRecorder.current.stream.getTracks().forEach((track) => track.stop());
+  };
+
+  const handleSendMessage = () => {
+    onSendMessage({ text: messageValue, attachment: messageAttachment });
+    setMessageAttachment(null);
+    setMessageValue("");
+  };
 
   return <div></div>;
 };
