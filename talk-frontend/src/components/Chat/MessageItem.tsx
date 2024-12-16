7465b40ca03b57d4b4704f54683f81452f0e5e8e
Renan <renanclaude.rcg@gmail.com>
2024-12-16 13:16:57 -0300

    Creating the component FileMessage for the future component MessageItem

---
+++ talk-frontend/src/components/Chat/MessageItem.tsx
@@ -0,0 +1,38 @@
+import { FileAttachment } from "@/types/Attachment";
+import { Message } from "@/types/Message";
+import { FileText } from "lucide-react";
+
+type MessageItemProps = {
+  data: Message;
+  onDelete: (messageId: number) => void;
+};
+
+export const FileMessage = ({ data }: { data: FileAttachment }) => (
+  <div className="flex items-center">
+    <a href={data.src} target="_blank">
+      {data.content_type.startsWith("image/") ? (
+        <img
+          className="md:max-w-96 h-80 object-cover rounded-md"
+          src={data.src}
+          alt={data.name}
+        />
+      ) : data.content_type.startsWith("video/") ? (
+        <video
+          className="max-w-96 h-80 object-cover rounded-md"
+          src={data.src}
+          controls={true}
+        />
+      ) : (
+        <div className="flex items-center gap-3.5 py-1 px-2.5">
+          <FileText className="size-7" />
+          <div>
+            <span className="font-bold">{`${data.name}.${data.extension}`}</span>
+            <p className="text-sm">
+              {data.size} - {data.content_type}
+            </p>
+          </div>
+        </div>
+      )}
+    </a>
+  </div>
+);
